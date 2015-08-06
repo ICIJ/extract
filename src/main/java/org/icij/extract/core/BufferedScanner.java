faf86ac69096771c651829dfc35af5b68f854c6a
Matthew Caruana Galizia <mattcg@gmail.com>
2015-08-05 19:19:05 -0600

    Internally, an ArrayBlockingQueue uses a lock while draining using #drainTo that causes all other operations in parallel threads to block. To avoid causing #queue to block while draining, use a separate thread which drains continuously.

--- src/main/java/org/icij/extract/core/BufferedScanner.java
+++ src/main/java/org/icij/extract/core/BufferedScanner.java
@@ -16,8 +16,8 @@ import java.nio.file.Path;
 
 /**
  * An implementation of {@link Scanner} which pushes encountered file paths into a
- * given buffer, then draining that buffer to a given queue when the buffer nears
- * capacity. 
+ * given or managed buffer, while a separate thread drains that buffer to the
+ * slow buffer.
  *
  * Use this class when the underlying queue suffers from latency that would slow
  * down an otherwise fast directory scanning operation. Otherwise use the regular
@@ -26,27 +26,26 @@ import java.nio.file.Path;
  * Paths are pushed into the buffer synchronously and if it is bounded, only
  * when a space becomes available.
  *
- * The treshold at which draining occurs is defined as 60% of the buffer's
- * capacity. Draining is an asynchronous operation that runs on a background
- * thread.
- *
  * This implementation is thread-safe.
  *
  * @since 1.0.0-beta
  */
 public class BufferedScanner extends Scanner {
 
-	private final BlockingQueue<String> buffer;
-	private final ExecutorService drainer = Executors.newSingleThreadExecutor();
-	private final AtomicBoolean draining = new AtomicBoolean();
-	private final int threshold;
+	/**
+	 * The "poison pill" which is signify to the worker that it should stop.
+	 */
+	protected static final String POISON_PILL = "\0";
+
+	protected final BlockingQueue<String> buffer;
+	protected final ExecutorService drainer = Executors.newSingleThreadExecutor();
 
 	/**
 	 * Creates a {@code BufferedScanner} that buffers all results from the
 	 * the scanner in a {@link BlockingQueue}, which should be bounded.
 	 *
-	 * When the internal queue is nearing capacity, a separate thread is
-	 * started which drains it to the underlying queue.
+	 * On instantiation, a separate thread is started which drains it to the
+	 * underlying queue.
 	 *
 	 * @param logger logger
 	 * @param queue results from the scanner will be put on this queue
@@ -56,32 +55,34 @@ public class BufferedScanner extends Scanner {
 		final BlockingQueue<String> buffer) {
 		super(logger, queue);
 		this.buffer = buffer;
-		this.threshold = (int) Math.ceil((double) buffer.remainingCapacity() / 1.6);
+		drainer.execute(new DrainingWorker());
 	}
 
+	/**
+	 * Creates a {@code BufferedScanner} with a buffer of the given size.
+	 *
+	 * @param logger logger
+	 * @param queue results from the scanner will be put on this queue
+	 * @param size the desired buffer capacity
+	 */
 	public BufferedScanner(final Logger logger, final BlockingQueue<String> queue,
 		final int size) {
 		this(logger, queue, new ArrayBlockingQueue<String>(size));
 	}
 
 	@Override
-	protected void accept(final Path file) throws InterruptedException {
+	protected void queue(final Path file) throws InterruptedException {
 		buffer.put(file.toString());
-		if (buffer.size() > threshold && draining.compareAndSet(false, true)) {
-			drainer.execute(new DrainingTask());
-		}
 	}
 
 	@Override
-	public void awaitTermination() throws InterruptedException {
-		super.awaitTermination();
+	public void finish() throws InterruptedException {
+		super.finish();
 
-		// When the scanner finishes, flush the buffer.
-		if (buffer.size() > 0 && draining.compareAndSet(false, true)) {
-			drainer.execute(new DrainingTask());
-		}
-
+		// When the scanner finishes, send a poison pill to the drainer
+		// and shut it down.
 		drainer.shutdown();
+		buffer.put(POISON_PILL);
 		do {
 			logger.info("Awaiting completion of drainer.");
 		} while (!drainer.awaitTermination(60, TimeUnit.SECONDS));
@@ -89,25 +90,34 @@ public class BufferedScanner extends Scanner {
 
 	/**
 	 * Runnable task that drains the internal buffer to an underlying queue.
-	 *
-	 * An implementation of {@link Callable} that returns the number of items
-	 * drained as its result.
 	 */
-	protected class DrainingTask implements Runnable, Callable<Integer> {
+	protected class DrainingWorker implements Runnable {
 
-		@Override
-		public void run() {
-			try {
-				call();
-			} catch (Exception e) {
-				logger.log(Level.SEVERE, "Exception while draining to slow queue.", e);
+		/**
+		 * Drains the queue in a loop until the current thread is interrupted
+		 * or the buffer is poisoned.
+		 */
+		private void loop() throws Exception {
+			while (!Thread.currentThread().isInterrupted()) {
+				String file = buffer.take();
+
+				if (!POISON_PILL.equals(file)) {
+					queue.put(file);
+				} else {
+					break;
+				}
 			}
 		}
 
 		@Override
-		public Integer call() throws Exception {
-			logger.info("Scanner draining to slow queue.");
-			return new Integer(buffer.drainTo(queue));
+		public void run() {
+			try {
+				loop();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, "Exception while draining to queue.", e);
+			}
 		}
 	}
 }
