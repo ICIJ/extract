13c706a59c49ba1e84e450fb1b334126413fc8cb
Matthew Caruana Galizia <mattcg@gmail.com>
2017-02-09 14:21:12 +0100

    Fix containers not getting written to disk

--- src/main/java/org/icij/extract/extractor/EmbedSpawner.java
+++ src/main/java/org/icij/extract/extractor/EmbedSpawner.java
@@ -1,5 +1,6 @@
 package org.icij.extract.extractor;
 
+import org.apache.poi.poifs.filesystem.*;
 import org.apache.tika.io.TemporaryResources;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
@@ -34,6 +35,7 @@ public class EmbedSpawner extends EmbedParser {
 	private ContentHandler originalHandler = null;
 	private final Function<Writer, ContentHandler> handlerFunction;
 	private LinkedList<Document> stack = new LinkedList<>();
+	private int untitled = 0;
 
 	EmbedSpawner(final Document root, final TemporaryResources tmp, final ParseContext context, final Path output,
 	             final Function<Writer, ContentHandler> handlerFunction) {
@@ -68,67 +70,112 @@ public class EmbedSpawner extends EmbedParser {
 				.get(TikaCoreProperties.EMBEDDED_RESOURCE_TYPE))) {
 			delegateParsing(input, embedHandler, metadata);
 		} else {
-			final Path parsedOutputPath = tmp.createTempFile();
-			final String name = metadata.get(Metadata.RESOURCE_NAME_KEY);
-
-			final Writer writer = Files.newBufferedWriter(parsedOutputPath, StandardCharsets.UTF_8);
-			final ContentHandler teeHandler = new TeeContentHandler(embedHandler, handlerFunction.apply(writer));
-
-			final EmbeddedDocument embed = stack.getLast().addEmbed(metadata);
-			stack.add(embed);
-
-			// Use temporary resources to copy formatted output from the content handler to a temporary file.
-			// Call setReader on the embed object with a plain reader for this temp file.
-			// When all parsing finishes, close temporary resources.
-			// Note that getPath should still return the path to the original file.
-			embed.setReader(() -> Files.newBufferedReader(parsedOutputPath, StandardCharsets.UTF_8));
-
-			final Path embedFilePath;
-
-			// Spool the file to disk, if needed, so that it can be copied. This needs to be done before parsing starts.
-			if (null != output) {
-				embedFilePath = TikaInputStream.get(input, tmp).getPath();
-			} else {
-				embedFilePath = null;
-			}
-
-			try {
-				delegateParsing(input, teeHandler, metadata);
-			} catch (Exception e) {
-
-				// Note that even on exception, the document is intentionally NOT removed from the parent.
-				logger.error("Unable to parse embedded document: \"{}\" (in \"{}\").", name, root, e);
-
-				// TODO: Change to TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM in Tika 1.15.
-				metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_WARNING, ExceptionUtils.getFilteredStackTrace(e));
-			} finally {
-				stack.removeLast();
-				writer.flush();
-				writer.close();
-			}
-
-			// Write the embed file to the given output directory.
-			if (null != embedFilePath) {
-				long copied;
-
-				try (final OutputStream copy = Files.newOutputStream(output.resolve(embed.getId()), StandardOpenOption
-						.CREATE_NEW)) {
-					copied = Files.copy(embedFilePath, copy);
-				} catch (FileAlreadyExistsException e) {
-					copied = -1;
-				}
-
-				if (0 == copied) {
-					logger.warn("No bytes copied for embedded document \"{}\" in \"{}\". "
-							+ "This could indicate a downstream error.", name, root);
-				} else if (-1 == copied) {
-					logger.info("Temporary file for document \"{}\" in \"{}\" already exists.", name, root);
-				}
-			}
+			spawnEmbedded(input, embedHandler, metadata);
 		}
 
 		if (outputHtml) {
 			writeEnd(originalHandler);
+		}
+	}
+
+	private void spawnEmbedded(final InputStream input, final ContentHandler handler, final Metadata metadata) throws
+			IOException {
+		final Path parsedOutputPath = tmp.createTempFile();
+		String name = metadata.get(Metadata.RESOURCE_NAME_KEY);
+
+		final Writer writer = Files.newBufferedWriter(parsedOutputPath, StandardCharsets.UTF_8);
+		final ContentHandler teeHandler = new TeeContentHandler(handler, handlerFunction.apply(writer));
+
+		final EmbeddedDocument embed = stack.getLast().addEmbed(metadata);
+		stack.add(embed);
+
+		// Use temporary resources to copy formatted output from the content handler to a temporary file.
+		// Call setReader on the embed object with a plain reader for this temp file.
+		// When all parsing finishes, close temporary resources.
+		// Note that getPath should still return the path to the original file.
+		embed.setReader(() -> Files.newBufferedReader(parsedOutputPath, StandardCharsets.UTF_8));
+
+		if (null == name || name.isEmpty()) {
+			name = String.format("untitled_%d", ++untitled);
+		}
+
+		if (null != output) {
+
+			// Trigger spooling of the file to disk so that it can be copied.
+			// This needs to be done before parsing starts.
+			TikaInputStream.get(input, tmp).getFile();
+		}
+
+		try {
+			delegateParsing(input, teeHandler, metadata);
+		} catch (Exception e) {
+
+			// Note that even on exception, the document is intentionally NOT removed from the parent.
+			logger.error("Unable to parse embedded document: \"{}\" (in \"{}\").", name, root, e);
+
+			// TODO: Change to TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM in Tika 1.15.
+			metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_WARNING, ExceptionUtils.getFilteredStackTrace(e));
+		} finally {
+			stack.removeLast();
+			writer.close();
+		}
+
+		// Write the embed file to the given output directory.
+		if (null != output) {
+			writeEmbed(input, embed, name);
+		}
+	}
+
+	private void writeEmbed(final InputStream input, final EmbeddedDocument embed, final String name) throws IOException {
+		long copied;
+		Path source;
+
+		final TikaInputStream tis = TikaInputStream.get(input, tmp);
+		final Object container = tis.getOpenContainer();
+
+		// If the input is a container, write it to a temporary file so that it can then be copied atomically.
+		// This happens with, for example, an Outlook Message that is an attachment of another Outlook Message.
+		if (container instanceof DirectoryEntry) {
+			final POIFSFileSystem fs = new POIFSFileSystem();
+
+			source = tmp.createTempFile();
+			saveEntries((DirectoryEntry) container, fs.getRoot());
+
+			try (final OutputStream output = Files.newOutputStream(source)) {
+				fs.writeFilesystem(output);
+			}
+		} else {
+			source = tis.getPath();
+		}
+
+		try (final OutputStream copy = Files.newOutputStream(output.resolve(embed.getId()),
+				StandardOpenOption.CREATE_NEW)) {
+			copied = Files.copy(source, copy);
+		} catch (FileAlreadyExistsException e) {
+			copied = -1;
+		}
+
+		if (0 == copied) {
+			logger.warn("No bytes copied for embedded document \"{}\" in \"{}\". "
+					+ "This could indicate a downstream error.", name, root);
+		} else if (-1 == copied) {
+			logger.info("Temporary file for document \"{}\" in \"{}\" already exists.", name, root);
+		}
+	}
+
+	private void saveEntries(final DirectoryEntry source, final DirectoryEntry destination) throws IOException {
+		for (Entry entry : source) {
+			if (entry instanceof DirectoryEntry) {
+
+				// Recursively save sub-entries.
+				saveEntries((DirectoryEntry) entry, destination.createDirectory(entry.getName()));
+			} else {
+
+				// Copy the entry.
+				try (final InputStream contents = new DocumentInputStream((DocumentEntry) entry)) {
+					destination.createDocument(entry.getName(), contents);
+				}
+			}
 		}
 	}
 }
