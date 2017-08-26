935a0d9b40342ae9a54b9ac26346f629b67b8ccd
Matthew Caruana Galizia <mattcg@gmail.com>
2017-08-26 15:46:52 +0200

    Try and read the length of the stream if it has no size set

--- src/main/java/org/icij/extract/parser/EmptyFileParser.java
+++ src/main/java/org/icij/extract/parser/EmptyFileParser.java
@@ -1,6 +1,7 @@
 package org.icij.extract.parser;
 
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.*;
@@ -27,11 +28,22 @@ public class EmptyFileParser extends AbstractParser {
 	public void parse(final InputStream stream, final ContentHandler handler, final Metadata metadata,
 	                  final ParseContext context) throws SAXException, IOException, TikaException {
 		final Parser parser;
-		final String size = metadata.get(Metadata.CONTENT_LENGTH);
+		final long size;
+		String value = metadata.get(Metadata.CONTENT_LENGTH);
+
+		if (null != value && !value.isEmpty()) {
+			size = Long.valueOf(value);
+		} else {
+			try (final TikaInputStream tis = TikaInputStream.get(stream)) {
+				size = tis.getLength();
+			}
+
+			metadata.set(Metadata.CONTENT_LENGTH, Long.toString(size));
+		}
 
 		// If the file is not empty, throw a parse error.
 		// Otherwise, output an empty document.
-		if (null == size || Long.valueOf(size) > 0) {
+		if (size > 0) {
 			parser = ErrorParser.INSTANCE;
 		} else {
 			metadata.set(Metadata.CONTENT_TYPE, "application/octet-stream");
