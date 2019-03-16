dd0427d77af19e60e4bfce52602722c20836004c
Bruno Thomas <bruno@barreverte.fr>
2019-03-16 17:05:31 +0000

    [test] adds a test for queue filter

--- extract-lib/src/test/java/org/icij/extract/redis/RedisDocumentQueueTest.java
+++ extract-lib/src/test/java/org/icij/extract/redis/RedisDocumentQueueTest.java
@@ -1,5 +1,6 @@
 package org.icij.extract.redis;
 
+import org.icij.extract.QueueFilterBuilder;
 import org.icij.extract.document.DocumentFactory;
 import org.icij.extract.document.PathIdentifier;
 import org.icij.task.Options;
@@ -7,6 +8,7 @@ import org.junit.After;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static java.nio.file.Paths.get;
 import static org.fest.assertions.Assertions.assertThat;
@@ -27,6 +29,21 @@ public class RedisDocumentQueueTest {
         assertThat(queue.removeDuplicatePaths()).isEqualTo(1);
 
         assertThat(queue.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void testRemoveExtracted() throws Exception {
+        queue.put(factory.create(get("/foo/bar")));
+        queue.put(factory.create(get("/foo/baz")));
+
+        RedisDocumentQueue filtered = (RedisDocumentQueue) new QueueFilterBuilder()
+                .filter(queue)
+                .with(() -> Stream.of(get("/foo/bar")))
+                .execute();
+
+        assertThat(filtered.getName()).isEqualTo(queue.getName() + ":filtered");
+        assertThat(filtered.size()).isEqualTo(1);
+        filtered.delete();
     }
 
     @After public void tearDown() { queue.delete();}
