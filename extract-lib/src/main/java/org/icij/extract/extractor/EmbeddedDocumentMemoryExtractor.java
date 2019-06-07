117d8f32e4d859db31af728f2e48b93938347d49
Bruno Thomas <bruno@barreverte.fr>
2019-06-07 07:25:26 +0000

    [fix] raw content extraction shoud be compatible with extract

--- extract-lib/src/main/java/org/icij/extract/extractor/EmbeddedDocumentMemoryExtractor.java
+++ extract-lib/src/main/java/org/icij/extract/extractor/EmbeddedDocumentMemoryExtractor.java
@@ -1,5 +1,6 @@
 package org.icij.extract.extractor;
 
+import com.sun.istack.internal.NotNull;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.extractor.ParsingEmbeddedDocumentExtractor;
 import org.apache.tika.metadata.Metadata;
@@ -8,73 +9,105 @@ import org.apache.tika.parser.DigestingParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
-import org.icij.extract.document.Identifier;
+import org.icij.extract.document.DigestIdentifier;
+import org.icij.extract.document.EmbeddedTikaDocument;
+import org.icij.extract.document.TikaDocument;
 import org.icij.extract.document.TikaDocumentSource;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+
+import static java.util.Optional.ofNullable;
 
 public class EmbeddedDocumentMemoryExtractor {
     private final Parser parser;
-    private final String digesterModifier;
-    private final String digesterAlgorithm;
+    private final DigestingParser.Digester digester;
+    private final String algorithm;
 
-    EmbeddedDocumentMemoryExtractor(final String digesterModifier, final String digesterAlgorithm) {
-        this.digesterModifier = digesterModifier;
-        this.digesterAlgorithm = digesterAlgorithm;
-        parser = new DigestingParser(new AutoDetectParser(), new UpdatableDigester(digesterModifier, digesterAlgorithm));
+    public EmbeddedDocumentMemoryExtractor(final UpdatableDigester digester) {
+        this(digester, digester.algorithm);
     }
 
-    public TikaDocumentSource extract(final InputStream stream, final String documentDigest) throws SAXException, TikaException, IOException {
-        Metadata metadata = new Metadata();
+    public EmbeddedDocumentMemoryExtractor(final DigestingParser.Digester digester, String algorithm) {
+        parser = new DigestingParser(new AutoDetectParser(), digester);
+        this.digester = digester;
+        this.algorithm = algorithm;
+    }
+
+    public TikaDocumentSource extract(final TikaDocument rootDocument, final String embeddedDocumentDigest) throws SAXException, TikaException, IOException {
         ParseContext context = new ParseContext();
         ContentHandler handler = new BodyContentHandler(-1);
         context.set(Parser.class, parser);
 
-        DigestEmbeddedDocumentExtractor extractor = new DigestEmbeddedDocumentExtractor(documentDigest, context,
-                new UpdatableDigester(digesterModifier, digesterAlgorithm));
+        DigestEmbeddedDocumentExtractor extractor = new DigestEmbeddedDocumentExtractor(rootDocument, embeddedDocumentDigest, context, digester, algorithm);
         context.set(org.apache.tika.extractor.EmbeddedDocumentExtractor.class, extractor);
 
-        parser.parse(stream, handler, metadata, context);
+        parser.parse(new FileInputStream(rootDocument.getPath().toFile()), handler, rootDocument.getMetadata(), context);
 
         return extractor.getDocument();
     }
 
     static class DigestEmbeddedDocumentExtractor extends ParsingEmbeddedDocumentExtractor {
-        private final String digest;
+        private final String digestToFind;
         private final ParseContext context;
-        private final UpdatableDigester digester;
+        private final DigestingParser.Digester digester;
+        private final String algorithm;
         private TikaDocumentSource document = null;
+        private LinkedList<TikaDocument> documentStack = new LinkedList<>();
 
-        private DigestEmbeddedDocumentExtractor(final String digest, ParseContext context,UpdatableDigester digester) {
+        private DigestEmbeddedDocumentExtractor(final TikaDocument rootDocument, final String digest, ParseContext context, DigestingParser.Digester digester, String algorithm) {
             super(context);
-            this.digest = digest;
+            this.digestToFind = digest;
             this.context = context;
             this.digester = digester;
+            this.algorithm = algorithm;
+            this.documentStack.add(rootDocument);
         }
 
         @Override
         public void parseEmbedded(InputStream stream, ContentHandler handler, Metadata metadata, boolean outputHtml) throws IOException, SAXException {
             if (document != null) return;
+            EmbeddedTikaDocument embed = this.documentStack.getLast().addEmbed(metadata);
 
             digester.digest(stream, metadata, context);
-            if (digest.equals(metadata.get(Identifier.getKey(digester.algorithm)))) {
+            String digest;
+            try {
+                digest = new DigestIdentifier(algorithm, Charset.defaultCharset()).generateForEmbed(embed);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+            if (digestToFind.equals(digest)) {
                 ByteArrayOutputStream buffer = new ByteArrayOutputStream();
                 int nbTmpBytesRead;
-                for(byte[] tmp = new byte[8192]; (nbTmpBytesRead = stream.read(tmp)) > 0;) {
+                for (byte[] tmp = new byte[8192]; (nbTmpBytesRead = stream.read(tmp)) > 0; ) {
                     buffer.write(tmp, 0, nbTmpBytesRead);
                 }
                 this.document = new TikaDocumentSource(metadata, buffer.toByteArray());
             } else {
+                this.documentStack.add(embed);
                 super.parseEmbedded(stream, handler, metadata, outputHtml);
+                this.documentStack.removeLast();
             }
         }
 
+        @NotNull
         public TikaDocumentSource getDocument() {
-            return document;
+            return ofNullable(document).orElseThrow(() ->
+                    new ContentNotFoundException(documentStack.get(0).getPath().toString(), digestToFind)
+            );
+        }
+    }
+
+    static class ContentNotFoundException extends NullPointerException {
+        ContentNotFoundException(String rootId, String embedId) {
+            super("<" + embedId + "> embedded document not found in root document " + rootId);
         }
     }
 }
--- extract-lib/src/test/java/org/icij/extract/extractor/EmbeddedDocumentMemoryExtractorTest.java
+++ extract-lib/src/test/java/org/icij/extract/extractor/EmbeddedDocumentMemoryExtractorTest.java
@@ -1,19 +1,45 @@
 package org.icij.extract.extractor;
 
+import org.icij.extract.document.DigestIdentifier;
+import org.icij.extract.document.DocumentFactory;
+import org.icij.extract.document.TikaDocument;
 import org.icij.extract.document.TikaDocumentSource;
+import org.icij.extract.extractor.EmbeddedDocumentMemoryExtractor.ContentNotFoundException;
+import org.icij.spewer.Spewer;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.io.InputStream;
+import java.io.Reader;
+import java.nio.charset.Charset;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
 
 public class EmbeddedDocumentMemoryExtractorTest {
+    private TikaDocument tikaDocument;
+
+    @Before
+    public void setUp() throws Exception {
+        tikaDocument = new DocumentFactory().withIdentifier(new DigestIdentifier("SHA-256", Charset.defaultCharset())).
+                create(getClass().getResource("/documents/recursive_embedded.docx"));
+    }
+
+    @Test
+    public void test_embedded_file_not_found() throws Exception {
+        try {
+            new EmbeddedDocumentMemoryExtractor(new UpdatableDigester("prj", "SHA-256")).
+                    extract(tikaDocument, "unknownDigest");
+            fail("NullPointerException should have been thrown");
+        } catch (ContentNotFoundException npe) {
+            assertThat(npe).hasMessage("<unknownDigest> embedded document not found in " +
+                    "root document /home/dev/src/extract/extract-lib/target/test-classes/documents/recursive_embedded.docx");
+        }
+    }
+
     @Test
     public void test_embedded_file_extraction_level_1() throws Exception {
-        InputStream stream = getClass().getResourceAsStream("/documents/recursive_embedded.docx");
-
-        TikaDocumentSource emfImage = new EmbeddedDocumentMemoryExtractor("prj", "SHA-256").
-                extract(stream, "c15ee2aa9c703e47d86993345ed5567c46441a39fa29a26a860af490c251564e");
+        TikaDocumentSource emfImage = new EmbeddedDocumentMemoryExtractor(new UpdatableDigester("prj", "SHA-256")).
+                extract(tikaDocument, "1eeb334ca60c61baca50b9df851b60c52b856c727932d0d1cae4e56a34190e7e");
 
         assertThat(emfImage).isNotNull();
         assertThat(emfImage.content).hasSize(4992);
@@ -21,10 +47,8 @@ public class EmbeddedDocumentMemoryExtractorTest {
 
     @Test
     public void test_embedded_file_extraction_level_2() throws Exception {
-        InputStream stream = getClass().getResourceAsStream("/documents/recursive_embedded.docx");
-
-        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor("prj", "SHA-256").
-                extract(stream, "b67d5e9b7876b9345c2a7d611ec4e7a2c5c143ab97267b42ef698f062abcabb4");
+        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor(new UpdatableDigester("prj", "SHA-256")).
+                extract(tikaDocument, "13d7b88767d478c03a3f9b01649297f254b3f0845ca1728658d7f7b922d28a32");
 
         assertThat(textContent).isNotNull();
         assertThat(new String(textContent.content)).isEqualTo("embed_1a");
@@ -32,10 +56,8 @@ public class EmbeddedDocumentMemoryExtractorTest {
 
     @Test
     public void test_embedded_file_extraction_level_4() throws Exception {
-        InputStream stream = getClass().getResourceAsStream("/documents/recursive_embedded.docx");
-
-        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor("prj", "SHA-256").
-                extract(stream, "7351c5258595692cc9813926218be0cd1bcaa6bc374d89a74b6266bb9a11d398");
+        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor(new UpdatableDigester("prj", "SHA-256")).
+                extract(tikaDocument, "c6eb226439d6a1a38ad23d69a98b2f321024eeb9ba32cd0238cc39a3d44b5130");
 
         assertThat(textContent).isNotNull();
         assertThat(new String(textContent.content)).isEqualTo("embed_4");
@@ -43,12 +65,29 @@ public class EmbeddedDocumentMemoryExtractorTest {
 
     @Test
     public void test_embedded_file_extraction_level_2_sha384() throws Exception {
-        InputStream stream = getClass().getResourceAsStream("/documents/recursive_embedded.docx");
-
-        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor("prj", "SHA-384").
-                extract(stream, "50a4893b3f4b9390061c28d09db0650ef9b30b3d32c3cc7b56349df5a6db0dc6707fd4bb7a9b5c6b0b38c1ce6b990c08");
+        TikaDocument tikaDocument384 = new DocumentFactory().withIdentifier(new DigestIdentifier("SHA-384", Charset.defaultCharset())).
+                        create(getClass().getResource("/documents/recursive_embedded.docx"));
+        TikaDocumentSource textContent = new EmbeddedDocumentMemoryExtractor(new UpdatableDigester("prj", "SHA-384")).
+                extract(tikaDocument384, "ad1892526e4c1c1c967390da3a8354b6926b03680156d7d76274e6971248be965bc15998a0260f19b801012227f03fae");
 
         assertThat(textContent).isNotNull();
         assertThat(new String(textContent.content)).isEqualTo("embed_2b");
+    }
+
+    @Test
+    public void test_embedded_file_content_extraction_should_have_same_hashes_than_extracted_docs() throws Exception {
+        Extractor extractor = new Extractor();
+        extractor.setDigester(new UpdatableDigester("prj", "SHA-256"));
+        try (Reader reader = extractor.extract(tikaDocument)) {
+            Spewer.toString(reader);
+        }
+
+        EmbeddedDocumentMemoryExtractor contentExtractor = new EmbeddedDocumentMemoryExtractor(
+                new UpdatableDigester("prj", "SHA-256"));
+
+        assertThat(tikaDocument.getEmbeds()).hasSize(2);
+        assertThat(contentExtractor.extract(tikaDocument, tikaDocument.getEmbeds().get(0).getId())).isNotNull();
+        assertThat(contentExtractor.extract(tikaDocument, tikaDocument.getEmbeds().get(1).getId())).isNotNull();
+        assertThat(contentExtractor.extract(tikaDocument, tikaDocument.getEmbeds().get(1).getEmbeds().get(0).getId())).isNotNull();
     }
 }
